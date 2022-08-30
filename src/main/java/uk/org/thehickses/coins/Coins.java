16bc7307b7742913dde9441efe724f84fbd85691
Jeremy Hicks <jeremy@thehickses.org.uk>
2022-08-30 10:43:04 +0100

    Initial commit

---
+++ src/main/java/uk/org/thehickses/coins/Coins.java
@@ -0,0 +1,79 @@
+package uk.org.thehickses.coins;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+public class Coins
+{
+    public static void main(String[] args) throws Exception
+    {
+        IntStream.rangeClosed(2004, 2022)
+                .boxed()
+                .flatMap(new Coins()::parse)
+                .sorted(comp)
+                .forEach(System.out::println);
+    }
+
+    static final String baseUri = "https://www.ecb.europa.eu";
+    static final String yearPage = "/euro/coins/comm/html/comm_%d.en.html";
+    static final Comparator<CoinData> comp = Comparator.comparing(CoinData::country)
+            .thenComparing(CoinData::year)
+            .thenComparing(CoinData::seq, Comparator.reverseOrder());
+
+    static UnaryOperator<String> norm()
+    {
+        var mappings = Map.of("The Netherlands", "Netherlands");
+        return c -> mappings.getOrDefault(c, c);
+    }
+
+    static final UnaryOperator<String> normaliser = norm();
+
+    static record CoinData(String country, int year, int seq, String description, String imageUri)
+    {
+
+    }
+
+    Stream<CoinData> parse(int year)
+    {
+        try
+        {
+            var url = baseUri + yearPage.formatted(year);
+            var str = new URL(url).openStream();
+            var doc = Jsoup.parse(str, "UTF-8", url);
+            return doc.select(".box")
+                    .stream()
+                    .map(parser(year));
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Function<Element, CoinData> parser(int year)
+    {
+        AtomicInteger seq = new AtomicInteger();
+        return elem ->
+            {
+                var country = normaliser.apply(elem.selectFirst("h3")
+                        .text());
+                var description = elem.selectFirst("strong:contains(Feature:)")
+                        .parent()
+                        .ownText();
+                var imageUri = elem.selectFirst("img")
+                        .attr("src");
+                return new CoinData(country, year, seq.getAndIncrement(), description, imageUri);
+            };
+    }
+
+}
