00a6dd1fed53c7d93a24e95f553851f3077c50ac
Jeremy Hicks <jeremy@thehickses.org.uk>
2022-09-07 11:01:43 +0100

    Refactor

--- src/main/java/uk/org/thehickses/coins/Coins.java
+++ src/main/java/uk/org/thehickses/coins/Coins.java
@@ -7,13 +7,15 @@ public class Coins
 
     public static void main(String[] args)
     {
-        var defsByCountry = new DefinitiveCoinParser().parse().sorted()
+        var defsByCountry = new DefinitiveCoinParser().parse()
                 .collect(Collectors.groupingBy(DefinitiveCoinData::country));
         var commemsByCountry = new CommemorativeCoinParser().parse()
                 .sorted()
                 .collect(Collectors.groupingBy(CommemorativeCoinData::country));
         var outputter = new CoinPageOutputter();
-        defsByCountry.keySet().stream().forEach(c -> outputter.output(c, defsByCountry.get(c), commemsByCountry.get(c)));
+        defsByCountry.keySet()
+                .stream()
+                .forEach(c -> outputter.output(c, defsByCountry.get(c), commemsByCountry.get(c)));
     }
 
 }
